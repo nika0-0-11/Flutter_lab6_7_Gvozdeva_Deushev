59728abca94e12c7242100486a4270854aeb3bb5
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-05-06 14:39:20 +0300

    Add app icon

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -53,7 +53,7 @@ class _SlotMachineState
       );
       result =
           _symbos[_random.nextInt(
-            _symbos.length,
+            _symbos.leapp_icon.pngngth,
           )];
       onTick(result);
     }
